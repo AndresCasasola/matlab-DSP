69f8910386c4939207c0224842e32df6585c0315
AndresCasasola <andres97dominguez@gmail.com>
2019-04-09 14:10:11 +0200

    Working with SignalHeart

--- experiments/SignalHeart/SignalHeart.m
+++ experiments/SignalHeart/SignalHeart.m
@@ -1,3 +1,7 @@
+
+%%% In a ECG signal must be filtered:
+% 50Hz power line noise
+
 s = load('short1-240Hz');
 
 fs = 240;     % Hz
@@ -12,81 +16,82 @@ f = fs*[0:(length(s)/2)]/length(s);
 % Filtering and gain
 G = 1;                      % Gain
 f_lowcut = 30;               % Lowpass filter cutoff frequency
-f_highcut = 2300;           % Highpass filter cutoff frequency
-f_bandpasscut = [2000 2800];% Bandpass filter cutoff frequencies
+f_highcut = 15;           % Highpass filter cutoff frequency
+f_bandpasscut = [0.1 20];% Bandpass filter cutoff frequencies
 f_bandstopcut = [80 2200];  % Bandstop filter cutoff frequencies
 
 % PLOT RAW SIGNAL
 figure(1);
-% Signal
+% Signal in time
 subplot(2,1,1);
 plot(t, s, '-b');
 axis([min(t) max(t) 0 max(s)*1.2]);
 title('Signal');
 xlabel('Time (s)');
 ylabel('Amplitude');
-
+% Signal in frequency
 s_transform = fft(s);
 P2 = abs(s_transform/length(s));
 P1 = P2(1:length(s)/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 subplot(2,1,2);
 plot(f, P1, '-r');
-axis([min(f) max(f)-90 min(P1) max(P1)*1.2]);
+axis([min(f) max(f) min(P1) max(P1)*1.2]);
 title('Signal transform');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 
+% Preparing signal
+s=s.^2;
+s=detrend(s);
+s_filtered = s;
 
 % FILTERING AND AMPLIFYING
 % Filters normalized frequency calculation
 fn_lowcut = f_lowcut/(fs/2);      
-%fn_highcut = f_highcut/(fs/2);
-%fn_bandpasscut = f_bandpasscut/(fs/2);
-%fn_bandstopcut = f_bandstopcut/(fs/2);
+fn_highcut = f_highcut/(fs/2);
+fn_bandpasscut = f_bandpasscut/(fs/2);
+fn_bandstopcut = f_bandstopcut/(fs/2);
 
 % Lowpass filter
-[zhi,phi,khi] = butter(20,fn_lowcut,'low'); %% zhi: zeros, phi: poles, k: gain
+[zhi,phi,khi] = butter(4,fn_lowcut,'low'); %% zhi: zeros, phi: poles, k: gain
 soshi = zp2sos(zhi,phi,khi);
-s_filtered = sosfilt(soshi,s);
-%se_filtered = sosfilt(soshi,se);
+s_filtered = sosfilt(soshi,s_filtered);
 
 % Highpass filter
-%[zhi,phi,khi] = butter(20,fn_highcut,'high'); %% zhi: zeros, phi: poles, k: gain
+%[zhi,phi,khi] = butter(1,fn_highcut,'high'); %% zhi: zeros, phi: poles, k: gain
 %soshi = zp2sos(zhi,phi,khi);
-%sa_filtered = sosfilt(soshi,sa_filtered);
-%se_filtered = sosfilt(soshi,se_filtered);
+%s_filtered = sosfilt(soshi,s_filtered);
 
 % Anti Bandpass filter
 %[B,A] = butter(5,fn_bandstopcut, 'stop');
-%sgood_filtered = filter(B, A, sgood_filtered);
-%snoise_filtered = filter(B, A, snoise_filtered);
+%s_filtered = filter(B, A, s_filtered);
 
 % Bandpass filter
 %[B,A] = butter(5,fn_bandpasscut, 'bandpass');
-%sgood_filtered = filter(B, A, sgood_filtered);
-%snoise_filtered = filter(B, A, snoise_filtered);
+%s_filtered = filter(B, A, s_filtered);
 
 % Amplification
-%sa = G.*sa;
-%se = G.*se;
+%s_filtered = G.*s_filtered;
+%s_filtered = G.*s_filtered;
 
 % PLOT FILTERED SIGNAL
 figure(2);
-% Signal filtered
+% Signal filtered in time
 subplot(2,1,1);
 plot(t, s_filtered, '-b');
-axis([min(t) max(t) 0 max(s)*1.2]);
+axis([min(t) max(t) min(s_filtered) max(s_filtered)*1.2]);
 title('Signal filtered');
 xlabel('Time (s)');
 ylabel('Amplitude');
+% Signal filtered in frequency
 s_filtered_transform = fft(s_filtered);
 P2 = abs(s_filtered_transform/length(s));
 P1 = P2(1:length(s)/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 subplot(2,1,2);
 plot(f, P1, '-r');
-axis([min(f) max(f)-90 min(P1) max(P1)*1.2]);
+axis([min(f) max(f) min(P1) max(P1)*1.2]);
 title('Signal filtered transform');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
